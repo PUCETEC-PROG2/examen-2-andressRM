9ae5048f4a76002ed65290c2f186e83a23c8d136
Williams Andres Sierra Narvaez <wasierran@puce.edu.ec>
2024-07-13 10:21:17 -0500

    ..hkj.

--- movies/urls.py
+++ movies/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.movie_list, name='movie_list'),
+    path('movie/<int:movie_id>/', views.movie_detail, name='movie_detail'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+# Create your views here.
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 def movie_list(request):
     movies = Movie.objects.all()
     return render(request, 'movies/movie_list.html', {'movies': movies})
 
-
-# Create your views here.
+def movie_detail(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    return render(request, 'movies/movie_detail.html', {'movie': movie})
